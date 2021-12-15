f0d88a5a93c0ef433dace694c8b4c78049cc5458
Kostya <fubionlaj@gmail.com>
2021-12-15 21:48:23 +0300

    Первый коммит

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log(5 + +"5");
+
+let incr = 10,
+  decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(++incr, --decr);
+
+// console.log(5 % 2);
+
+// console.log(2 * 4 === 8);
+
+const isChecked = false,
+  isClosed = false;
+
+console.log(isChecked || !isClosed);
+
+console.log(2 + 2 * 2 !== "6");
