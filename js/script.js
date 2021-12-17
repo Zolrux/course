e2076b61368986ae95e13409593b00b4376f37e9
Kostya <fubionlaj@gmail.com>
2021-12-17 20:19:17 +0300

    Callback

--- js/script.js
+++ js/script.js
@@ -1,70 +1,26 @@
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
-
-  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
-  }
+function first() {
+  // Do something
+  setTimeout(function () {
+    console.log(1);
+  }, 500);
 }
 
-start();
-
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-
-function showMyDB() {
-  if (!personalMovieDB.privat) {
-    console.log(personalMovieDB);
-  }
+function second() {
+  console.log(2);
 }
 
-function rememberMyFilms() {
-  let i = 0;
-  while (i < 2) {
-    const lastFilm = prompt("Один из последних просмотренных фильмов?"),
-      rating = +prompt("Насколько оцените его?");
+first();
+second();
 
-    if (lastFilm != null && rating != null && lastFilm != "" && rating != "" && lastFilm.length < 50) {
-      personalMovieDB.movies[lastFilm] = rating;
-    } else {
-      i--;
-    }
-    i++;
-  }
+function learnJS(lang, callback) {
+  console.log("Я учу: " + lang);
+  callback();
 }
 
-rememberMyFilms();
-
-// showMyDB();
-
-function writeYourGenres() {
-  for (let i = 0; i < 3; i++) {
-    personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номеро ${i + 1}`);
-  }
+function done() {
+  console.log("Я прошел этот урок!");
 }
 
-writeYourGenres();
-
-showMyDB();
-
-function typeOfViewers() {
-  if (personalMovieDB.count < 10 && personalMovieDB.count > -1) {
-    console.log("Просмотрено довольно мало фильмов!");
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель!");
-  } else if (personalMovieDB.count > 30) {
-    console.log("Вы киноман");
-  } else {
-    console.log("Произошла ошибка!");
-  }
-}
-
-typeOfViewers();
+learnJS("javascript", done);
