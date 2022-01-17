cff0925afba53887269409b5e47bfb9f26635205
Kostya <fubionlaj@gmail.com>
2022-01-17 19:42:46 +0300

    Prototype

--- js/script.js
+++ js/script.js
@@ -1,97 +1,29 @@
 "use strict";
 
-let a = 5,
-  b = a;
+let str = "some";
+let strObj = new String(str);
 
-b = b + 5;
+// console.log(typeof str);
+// console.log(typeof strObj);
 
-// console.log(b);
-// console.log(a);
+// console.dir([1, 2, 3]);
 
-// const obj = {
-//   a: 5,
-//   b: 1,
-// };
-
-// const copy = obj; // Ссылка на обьект obj
-
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-function copy(mainObj) {
-  let objCopy = {};
-
-  let key;
-
-  for (key in mainObj) {
-    objCopy[key] = mainObj[key];
-  }
-
-  return objCopy;
-}
-
-let numbers = {
-  a: 2,
-  b: 5,
-  c: {
-    x: 7,
-    y: 4,
+const soldier = {
+  health: 400,
+  armor: 100,
+  sayHello: function () {
+    console.log("hello");
   },
 };
 
-const newNumbers = copy(numbers);
+const john = Object.create(soldier);
 
-newNumbers.a = 10;
-newNumbers.c.x = 10;
+// const john = {
+//   health: 100,
+// };
 
-// console.log(numbers);
-// console.log(newNumbers);
+// john.__proto__ = soldier; // Старый способ
 
-const add = {
-  d: 17,
-  e: 20,
-};
+// Object.setPrototypeOf(john, soldier);
 
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ["a", "b", "c"];
-const newArray = oldArray.slice();
-
-newArray[1] = "Hello";
-// console.log(newArray);
-// console.log(oldArray);
-
-const video = ["Yt", "Vimeo", "RT"];
-const blogs = ["WP", "LJ", "BL"];
-
-const internet = [...video, ...blogs, "vk", "facebook"];
-
-// console.log(internet);
-
-function log(a, b, c) {
-  console.log(a);
-  console.log(b);
-  console.log(c);
-}
-
-const num = [2, 5, 7];
-
-// log(...num);
-
-const array = ["a", "b"];
-
-const newArr = [...array];
-
-const q = {
-  one: 1,
-  two: 2,
-};
-
-const newObj = { ...q };
+console.log(john);
