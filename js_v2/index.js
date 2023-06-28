308c272deec1ca5e0120f876a0a924c117bad6ed
Kostya <fubionlaj@gmail.com>
2023-06-28 21:50:53 +0300

    Lecture: closure

--- js_v2/index.js
+++ js_v2/index.js
@@ -1,34 +1,13 @@
 'use strict';
 
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
-
-function sortStudentsByGroups(arr) {
-	const groups = [];
-	let endIndex = 0;
-	let countTeams = parseInt(arr.length / 3);
-	let studentsWithoutGroup = 'Оставшиеся студенты: ';
-
-	arr.sort();
-
-	// Формируем группу из 3-х человек
-	for (let i = 0; i < countTeams; i++) {
-		const group = arr.slice(endIndex, endIndex + 3);
-		groups.push(group);
-		endIndex += 3;
-	}
-
-	// Добавляем информацию про оставшихся студентов (если такие имеются)
-	if (endIndex < arr.length) {
-		const remainingStudents = arr.slice(endIndex).join(', ');
-		studentsWithoutGroup += remainingStudents;
-		groups.push(studentsWithoutGroup);
-	} else {
-		studentsWithoutGroup += '-';
-		groups.push(studentsWithoutGroup);
-	}
-
-	return groups;
+function createCounter() {
+	let counter = 0;
+	return function() {
+		return counter++;
+	};
 }
-
-const result = sortStudentsByGroups(students);
-console.log(result);
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+console.log('example increment', c1, c2, c3);
