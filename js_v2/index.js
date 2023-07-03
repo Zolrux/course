074f0fb139c78d0f320a9176de9f7c604f7cb188
Kostya <fubionlaj@gmail.com>
2023-07-03 21:09:18 +0300

    Lecture and Tasks: Object Date

--- js_v2/index.js
+++ js_v2/index.js
@@ -1,284 +1,147 @@
 'use strict';
 
-//  function pow(x, n) {
-//  	let result = 1;
-//  	for (let i = 0; i < n; i++) {
-//  		result *= x;
-//  	}
-//  	return result;
-//  }
+// const now = new Date('2023-07-03');
+// new Date.parse(now);
 
-function pow(x, n) {
-	if (n === 1) {
-		return x;
-	} else {
-		return x * pow(x, n - 1);
-	}
+// console.log(now.setHours(40));
+// console.log(now.setMinutes(40));
+// console.log(now);
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDay());
+// console.log(now.getDay());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+// let start = new Date();
+
+// for (let i = 0; i < 100000; i++) {
+// 	let some = i ** 3;
+// }
+// let end = new Date();
+
+// alert(`Цикл отработал за ${end - start}мс`);
+
+// 1)
+// alert(new Date(2012, 1, 20, 3, 12));
+
+// 2)
+// function getWeekDay(date) {
+// 	const arr = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+// 	return arr[date.getDay()];
+// switch (dt.getDay()) {
+// case 0: {
+// 	return 'ВС';
+// }
+// case 1: {
+// 	return 'ПН';
+// }
+// case 2: {
+// 	return 'ВТ';
+// }
+// case 3: {
+// 	return 'СР';
+// }
+// case 4: {
+// 	return 'ЧТ';
+// }
+// case 5: {
+// 	return 'ПТ';
+// }
+// case 6: {
+// 	return 'СБ';
+// }
+// }
+// }
+
+// alert(getWeekDay(new Date(2023, 6, 16)));
+
+// 3)
+
+// function getLocalDay(date) {
+// 	if (date.getDay() === 0) {
+// 		return 7;
+// 	}
+// 	return date.getDay();
+// }
+
+// let date = new Date(2023, 6, 3);
+// alert( getLocalDay(date) );  
+
+// 4)
+
+// function getDateAgo(date, day) {
+// 	const newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - day);
+// 	return newDate.getDate();
+
+// 	/* Лучше сделать через setDate(день) */
+// }
+
+// alert( getDateAgo(new Date(2015, 0, 2), 365));
+
+// 5)
+
+/* function getLastDayOfMonth(year, month) {
+	return new Date(year, month + 1, 0).getDate();
 }
 
-console.log(pow(2, 1));
-console.log(pow(2, 2));
-console.log(pow(2, 3));
-console.log(pow(2, 4));
+alert(getLastDayOfMonth(2020, 1)); */
 
-let students = {
-	js: [
-		{
-			name: 'John',
-			progress: 100,
-		},
-		{
-			name: 'Ivan',
-			progress: 60,
-		},
-	],
-	html: {
-		basic: [
-			{
-				name: 'Peter',
-				progress: 20,
-			},
-			{
-				name: 'Ann',
-				progress: 18,
-			},
-		],
+// 6) 
 
-		pro: [
-			{
-				name: 'Sam',
-				progress: 10,
-			},
-		],
+// function getSecondsToday() {
+// 	const now = Date.now();
+// 	const startDate = new Date().setHours(0, 0, 0);
+// 	const getTotalSeconds = Math.floor((now - startDate) / 1000);
+// 	return getTotalSeconds;
+// }
 
-		semi: {
-			students: [
-				{
-					name: 'test',
-					progress: 100,
-				},
-			],
-		},
-	},
-};
+// console.log(getSecondsToday());
 
-function getTotalProgressByIteration(data) {
-	let total = 0;
-	let students = 0;
+// 7)
 
-	for (let course of Object.values(data)) {
-		if (Array.isArray(course)) {
-			students += course.length;
+// function getSecondsToTomorrow() {
+// 	const secondsInDay = 60 * 60 * 24;
+// 	const now = new Date();
+// 	const timePassedHoursToSeconds = now.getHours() * 60 * 60;
+// 	const timePassedMinutesToSeconds = now.getMinutes() * 60;
+// 	const timePassedSeconds = now.getSeconds();
+// 	const timePassedTotalSeconds = timePassedHoursToSeconds + timePassedMinutesToSeconds + timePassedSeconds;
+// 	return secondsInDay - timePassedTotalSeconds;
+// }
 
-			for (let i = 0; i < course.length; i++) {
-				total += course[i].progress;
-			}
-		} else {
-			for (let subCourse of Object.values(course)) {
-				students += subCourse.length;
+// console.log(getSecondsToTomorrow());
 
-				for (let i = 0; i < subCourse.length; i++) {
-					total += subCourse[i].progress;
-				}
-			}
-		}
-	}
+// 8)
 
-	return total / students;
-}
-
-console.log(getTotalProgressByIteration(students));
-
-function getTotalProgressByRecursion(data) {
-	if (Array.isArray(data)) {
-		let total = 0;
-
-		for (let i = 0; i < data.length; i++) {
-			total += data[i].progress;
-		}
-
-		return [total, data.length];
-	} else {
-		let total = [0, 0];
-
-		for (let subData of Object.values(data)) {
-			const subDataArr = getTotalProgressByRecursion(subData);
-			total[0] += subDataArr[0];
-			total[1] += subDataArr[1];
-		}
-
-		return total;
-	}
-}
-
-const res = getTotalProgressByRecursion(students);
-
-console.log(res[0] / res[1]);
-
-function fibonacci(n) {
-	if (n <= 1) {
-		return n;
-	} else {
-		return fibonacci(n - 1) + fibonacci(n - 2);
-	}
-}
-
-console.log(fibonacci(3));
-
-function sumOfDigits(number) {
-	if (number < 10) {
-		return 1;
-	} else {
-		return 1 + Math.floor(sumOfDigits(number / 10));
-	}
-}
-
-const num1 = sumOfDigits(33);
-3;
-const num2 = sumOfDigits(1333);
-4;
-
-console.log(num1, num2);
-
-// ========================================================================================================================================================
-
-/* 
-Найдите наибольший общий делитель двух положительных чисел
-Напишите программу на JavaScript, чтобы найти наибольший общий делитель 
-(НОД) двух положительных чисел.
-*/
-
-let counter = 1;
-let resultNOD = 0;
-
-function findNOD(a, b) {
-	const endIteration = Math.min(a, b);
-	if (counter <= endIteration) {
-		const boolValue = a % counter === 0 && b % counter === 0;
-		if (boolValue) {
-			resultNOD = counter;
-		}
-		if (counter < endIteration) {
-			counter++;
-			findNOD(a, b);
-		}
-	}
-	if (counter === endIteration) {
-		return resultNOD;
-	}
-}
-
-console.log(findNOD(28, 15));
-
-//========================================================================================================================================================
-
-let gcd = function (a, b) {
-	if (!b) {
-		return a;
+function formatDate(date) {
+	const getPassedTimeInSeconds = (new Date() - date) / 1000;
+	if (getPassedTimeInSeconds < 1) {
+		return 'прямо сейчас';
 	}
 
-	return gcd(b, a % b);
-};
-console.log(gcd(125, 75));
-
-// ХОРОШИЙ ВАРИАНТ
-
-// ========================================================================================================================================================
-
-/* 
-Получите целые числа в диапазоне
-Напишите программу на JavaScript, чтобы получить целые числа в диапазоне (x, y).
-
-Пример: диапазон (10, 18)
-Ожидаемый результат: [11,12,13,14,15,16,17]
-*/
-
-const result = [];
-function getNumbersRange(min, max) {
-	if (min + 1 < max) {
-		result.push(min + 1);
-		return getNumbersRange(min + 1, max);
+	const getPassedTimeInMinutes = (new Date() - date) / 1000 / 60;
+	if (getPassedTimeInMinutes < 1) {
+		return `${Math.floor(getPassedTimeInMinutes * 60)} секунд назад`;
 	}
-}
 
-getNumbersRange(10, 18);
-console.log(result);
-
-// ========================================================================================================================================================
-
-/* 
-Вычислите сумму массива целых чисел
-Напишите программу на JavaScript для вычисления суммы массива целых чисел.
-
-Пример: let array = [2, 3, 4, 5, 6, 7]
-Ожидаемый результат: 27
-*/
-
-function getSumFromArr(arr, i = 0) {
-	if (i < arr.length) {
-		return arr[i] + getSumFromArr(arr, i + 1);
-	} else {
-		return 0;
+	const getPassedTimeInHours = (new Date() - date) / 1000 / 60 / 60;
+	if (getPassedTimeInHours < 1) {
+		return `${Math.floor(getPassedTimeInHours * 60)} минут назад`;
 	}
+
+	const convertValue = value => value < 10 ? `0${value}` : value;
+	const day = convertValue(date.getDate());
+	const month = convertValue(date.getMonth() + 1);
+	const year =  date.getFullYear();
+	const hours =  convertValue(date.getHours());
+	const minutes =  convertValue(date.getMinutes());
+	return `${day}.${month}.${year} ${hours}:${minutes}`;
 }
 
-let array = [2, 3, 4, 5, 6, 7];
-const sum = getSumFromArr(array);
-console.log(sum);
-
-// ========================================================================================================================================================
-
-/* 
-Вычислите экспоненту числа
-Напишите программу на JavaScript для вычисления экспоненты числа.
-
-Примечание: Показатель числа говорит о том, 
-сколько раз базовое число используется в качестве множителя. 
-8 2 = 8 x 8 = 64. Здесь 8 - основание, а 2 - показатель степени.
-*/
-
-function getPowNumber(num, n) {
-	if (n <= 1) return 1;
-	return num * getPowNumber(num, n - 1);
-}
-
-console.log(getPowNumber(5, 3));
-
-// ========================================================================================================================================================
-
-/* 
-Получить первые n чисел Фибоначчи
-Напишите программу на JavaScript, чтобы получить первые n чисел Фибоначчи.
-
-Примечание: Последовательность Фибоначчи - это последовательность чисел: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34,... 
-Каждое последующее число является суммой двух предыдущих.
-*/
-
-function fib(n) {
-	if (n === 0) return 0;
-	if (n === 1) return 1;
-	return fib(n - 1) + fib(n - 2);
-}
-
-for (let i = 0; i < 13; i++) {
-	console.log(fib(i));
-}
-
-// ========================================================================================================================================================
-
-/* 
-7. Проверьте, четное число или нет
-Напишите программу на JavaScript, чтобы проверить, четное число или нет.
-*/
-
-function isNumberEven(number) {
-	number = Math.abs(number);
-	if (number === 0) {
-		return true;
-	} else if (number === 1) {
-		return false;
-	}
-	return isNumberEven(number - 2);
-}
-
-console.log(isNumberEven(-96));
+alert(formatDate(new Date(new Date - 1)));
+alert(formatDate(new Date(new Date - 30 * 1000)));
+alert(formatDate(new Date(new Date - 5 * 60 * 1000)));
+alert(formatDate(new Date(new Date - 86400 * 1000)));
