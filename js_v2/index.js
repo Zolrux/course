8d70aac315459dd0243e9f7fbaaa6c9ca0175699
Kostya <fubionlaj@gmail.com>
2023-07-05 17:28:48 +0300

    Lecture and Tasks: Different objects observer

--- js_v2/index.js
+++ js_v2/index.js
@@ -1,147 +1,70 @@
 'use strict';
 
-// const now = new Date('2023-07-03');
-// new Date.parse(now);
+// const box = document.querySelector('.box');
 
-// console.log(now.setHours(40));
-// console.log(now.setMinutes(40));
-// console.log(now);
-// console.log(now.getFullYear());
-// console.log(now.getMonth());
-// console.log(now.getDay());
-// console.log(now.getDay());
-// console.log(now.getUTCHours());
+// let observer = new MutationObserver(mutationRecords => {
+// 	console.log(mutationRecords);
+// });
 
-// console.log(now.getTimezoneOffset());
-// console.log(now.getTime());
+// observer.observe(box, {
+// 	childList: true
+// });
 
-// let start = new Date();
+// observer.disconnect();
 
-// for (let i = 0; i < 100000; i++) {
-// 	let some = i ** 3;
-// }
-// let end = new Date();
+//========================================================================================================================================================
 
-// alert(`Цикл отработал за ${end - start}мс`);
+// const observer = new ResizeObserver(entries => {
+// 	console.log(entries);
+// });
 
-// 1)
-// alert(new Date(2012, 1, 20, 3, 12));
+// observer.observe(box);
 
-// 2)
-// function getWeekDay(date) {
-// 	const arr = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
-// 	return arr[date.getDay()];
-// switch (dt.getDay()) {
-// case 0: {
-// 	return 'ВС';
-// }
-// case 1: {
-// 	return 'ПН';
-// }
-// case 2: {
-// 	return 'ВТ';
-// }
-// case 3: {
-// 	return 'СР';
-// }
-// case 4: {
-// 	return 'ЧТ';
-// }
-// case 5: {
-// 	return 'ПТ';
-// }
-// case 6: {
-// 	return 'СБ';
-// }
-// }
-// }
+//========================================================================================================================================================
 
-// alert(getWeekDay(new Date(2023, 6, 16)));
+// const options = {
+// 	threshold: 0.5,
+// 	// rootMargin: '100px'
+// };
 
-// 3)
+// const observer = new IntersectionObserver(entries => {
+// 	console.log(entries);
+// }, options);
 
-// function getLocalDay(date) {
-// 	if (date.getDay() === 0) {
-// 		return 7;
-// 	}
-// 	return date.getDay();
-// }
+// observer.observe(box);
 
-// let date = new Date(2023, 6, 3);
-// alert( getLocalDay(date) );  
+//========================================================================================================================================================
 
-// 4)
+// Практика с Intersection
 
-// function getDateAgo(date, day) {
-// 	const newDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - day);
-// 	return newDate.getDate();
+let cards = document.querySelectorAll('.card');
 
-// 	/* Лучше сделать через setDate(день) */
-// }
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach(entry => {
+		entry.target.classList.toggle('showed', entry.isIntersecting);
+		if (entry.isIntersecting) observer.unobserve(entry.target);
 
-// alert( getDateAgo(new Date(2015, 0, 2), 365));
+		const lastCard = document.querySelector('.card:last-child');
+		if (entry.target === lastCard && entry.isIntersecting) {
+			cards = document.querySelectorAll('.card');
+			createCards(cards.length);
+		}
+	});
+}, {
+	threshold: 0.1,
+});
 
-// 5)
+cards.forEach(card => {
+	observer.observe(card);
+});
 
-/* function getLastDayOfMonth(year, month) {
-	return new Date(year, month + 1, 0).getDate();
+function createCards(startIndex) {
+	const wrapper = document.querySelector('.wrapper');
+	for (let i = startIndex; i < startIndex + 10; i++) {
+		const card = document.createElement('div');
+		card.classList.add('card');
+		card.textContent = `Я карточка ${i + 1}`;
+		observer.observe(card);
+		wrapper.append(card);
+	}
 }
-
-alert(getLastDayOfMonth(2020, 1)); */
-
-// 6) 
-
-// function getSecondsToday() {
-// 	const now = Date.now();
-// 	const startDate = new Date().setHours(0, 0, 0);
-// 	const getTotalSeconds = Math.floor((now - startDate) / 1000);
-// 	return getTotalSeconds;
-// }
-
-// console.log(getSecondsToday());
-
-// 7)
-
-// function getSecondsToTomorrow() {
-// 	const secondsInDay = 60 * 60 * 24;
-// 	const now = new Date();
-// 	const timePassedHoursToSeconds = now.getHours() * 60 * 60;
-// 	const timePassedMinutesToSeconds = now.getMinutes() * 60;
-// 	const timePassedSeconds = now.getSeconds();
-// 	const timePassedTotalSeconds = timePassedHoursToSeconds + timePassedMinutesToSeconds + timePassedSeconds;
-// 	return secondsInDay - timePassedTotalSeconds;
-// }
-
-// console.log(getSecondsToTomorrow());
-
-// 8)
-
-function formatDate(date) {
-	const getPassedTimeInSeconds = (new Date() - date) / 1000;
-	if (getPassedTimeInSeconds < 1) {
-		return 'прямо сейчас';
-	}
-
-	const getPassedTimeInMinutes = (new Date() - date) / 1000 / 60;
-	if (getPassedTimeInMinutes < 1) {
-		return `${Math.floor(getPassedTimeInMinutes * 60)} секунд назад`;
-	}
-
-	const getPassedTimeInHours = (new Date() - date) / 1000 / 60 / 60;
-	if (getPassedTimeInHours < 1) {
-		return `${Math.floor(getPassedTimeInHours * 60)} минут назад`;
-	}
-
-	const convertValue = value => value < 10 ? `0${value}` : value;
-	const day = convertValue(date.getDate());
-	const month = convertValue(date.getMonth() + 1);
-	const year =  date.getFullYear();
-	const hours =  convertValue(date.getHours());
-	const minutes =  convertValue(date.getMinutes());
-	return `${day}.${month}.${year} ${hours}:${minutes}`;
-}
-
-alert(formatDate(new Date(new Date - 1)));
-alert(formatDate(new Date(new Date - 30 * 1000)));
-alert(formatDate(new Date(new Date - 5 * 60 * 1000)));
-alert(formatDate(new Date(new Date - 86400 * 1000)));
