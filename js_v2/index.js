e3859d51829e3cfd2a3f907d86356935dd41fd88
Kostya <fubionlaj@gmail.com>
2023-07-07 18:05:55 +0300

    Lecture: Function constructor

--- js_v2/index.js
+++ js_v2/index.js
@@ -1,70 +1,25 @@
 'use strict';
 
-// const box = document.querySelector('.box');
-
-// let observer = new MutationObserver(mutationRecords => {
-// 	console.log(mutationRecords);
-// });
-
-// observer.observe(box, {
-// 	childList: true
-// });
-
-// observer.disconnect();
-
-//========================================================================================================================================================
-
-// const observer = new ResizeObserver(entries => {
-// 	console.log(entries);
-// });
-
-// observer.observe(box);
-
-//========================================================================================================================================================
-
-// const options = {
-// 	threshold: 0.5,
-// 	// rootMargin: '100px'
-// };
-
-// const observer = new IntersectionObserver(entries => {
-// 	console.log(entries);
-// }, options);
-
-// observer.observe(box);
-
-//========================================================================================================================================================
-
-// Практика с Intersection
-
-let cards = document.querySelectorAll('.card');
-
-const observer = new IntersectionObserver((entries) => {
-	entries.forEach(entry => {
-		entry.target.classList.toggle('showed', entry.isIntersecting);
-		if (entry.isIntersecting) observer.unobserve(entry.target);
-
-		const lastCard = document.querySelector('.card:last-child');
-		if (entry.target === lastCard && entry.isIntersecting) {
-			cards = document.querySelectorAll('.card');
-			createCards(cards.length);
-		}
-	});
-}, {
-	threshold: 0.1,
-});
-
-cards.forEach(card => {
-	observer.observe(card);
-});
-
-function createCards(startIndex) {
-	const wrapper = document.querySelector('.wrapper');
-	for (let i = startIndex; i < startIndex + 10; i++) {
-		const card = document.createElement('div');
-		card.classList.add('card');
-		card.textContent = `Я карточка ${i + 1}`;
-		observer.observe(card);
-		wrapper.append(card);
-	}
+function User(name, id) {
+	this.name = name;
+	this.id = id;
+	this.human = true;
+	this.hello = function() {
+		console.log(`Hello ${this.name}`);
+	};
 }
+
+User.prototype.exit = function () {
+	console.log(`Пользователь ${this.name} ушел`);
+};
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
