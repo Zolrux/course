04a255cef0850aea8f0d8e0fbe2fc03577d1d3ba
Kostya <fubionlaj@gmail.com>
2023-07-08 11:30:54 +0300

    Lecture: rest operator and default parameters

--- js_v2/index.js
+++ js_v2/index.js
@@ -1,33 +1,13 @@
 'use strict';
 
-class Rectangle {
-	constructor(height, width) {
-		this.height = height;
-		this.width = width;
-	}
+const log = function (a, b, ...rest) {
+	console.log(a, b, rest);
+};
 
-	calcArea() {
-		return this.height * this.width;
-	}
+log('basic', 'rest', 'operator', 'usage');
+
+function calcOrDouble(number, basis = 2) {
+	console.log(number * basis);
 }
 
-class ColoredRectangleWithText extends Rectangle {
-	constructor(height, width, text, bgColor) {
-		super(height, width);
-		this.text = text;
-		this.text = text;
-		this.bgColor = bgColor;
-	}
-	showMyProps() {
-		console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
-	}
-}
-
-// const square = new Rectangle(10, 10);
-// const long = new Rectangle(20, 100);
-// console.log(square.calcArea());
-// console.log(long.calcArea());
-
-const div = new ColoredRectangleWithText(25, 10, 'Hello world', 'red');
-div.showMyProps();
-console.log(div.calcArea());
+calcOrDouble(3);
