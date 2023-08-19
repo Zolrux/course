becc6cc1ead529960a84a5580970731b757332f2
Kostya <fubionlaj@gmail.com>
2023-08-19 16:19:51 +0300

    Lecture: array iteration methods

--- js_v2/index.js
+++ js_v2/index.js
@@ -1,55 +1,70 @@
 'use strict';
 
-document.body.innerHTML = 'Подождите, идет получение данных с сервера';
+// filter
 
-// GET request
+// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
 
-// let counter = 0;
+// const shortNames = names.filter(function(name) {
+// 	return name.length < 5;
+// });
 
+// console.log(shortNames);
 
-// fetch('https://jsonplaceholder.typicode.com/photos')
-// 	.then(response => {
-// 		if (response.ok) {
-// 			return response.json();
-// 		} else {
-// 			document.body.innerHTML = 'Что-то пошло не так...';
-// 		}
-// 	})
-// 	.then(json => {
-// 		document.body.innerHTML = '';
-// 		for (const {url} of json) {
-// 			if (counter < 50) {
-// 				const img = document.createElement('img');
-// 				img.src = url;
-// 				img.style.width = '100px';
-// 				document.body.append(img);
-// 				counter++;
-// 			}
-// 		}
-// 	})
-// 	.catch(error => {
-// 		document.body.innerHTML = 'Что-то пошло не так...';
-// 	});
+// map
 
+// let answers = ['IvAn', 'AnnA', 'Hello'];
 
-// POST request
+// answers = answers.map(item => item.toLowerCase());
 
-fetch('https://jsonplaceholder.typicode.com/posts', {
-	method: 'POST',
-	body: JSON.stringify({userId: 16, title: 'How are you?'}),
-	headers: {
-		'Content-type': 'application/json'
-	}
-})
-	.then(response => {
-		if (response.ok) {
-			document.body.innerHTML = 'Ответ от сервера успешно получен!';
-			return response.json();
-		}
-	})
-	.then(json => {
-		document.body.innerHTML += JSON.stringify(json);
-	})
-	.catch(error => {
-		document.body.innerHTML = 'Что-то пошло не так...';
-	});
+// console.log(answers);
+
+// every/some // Возвращают true / false
+
+// const some = [4, 5, 7];
+
+// console.log(some.some(item => typeof item === 'number'));
+// console.log(some.some(Number));
+
+// console.log(some.every(item => typeof item === 'number'));
+
+// reduce
+
+// const arr = [4, 5, 1, 3, 2, 6];
+
+// const result = arr.reduce((sum, current) => sum + current, 3);
+
+// console.log(result);
+
+// const arr = ['apple', 'pear', 'plum'];
+
+// const result = arr.reduce((sum, current) => `${sum}, ${current}`);
+
+// console.log(result);
+
+const obj = {
+	ivan: 'person',
+	ann: 'person',
+	dog: 'animal',
+	cat: 'animal'
+};
+
+const newArr = Object.entries(obj)
+	.filter(item => item[1] === 'person')
+	.map(item => item[0]);
+
+console.log(newArr);
+
+// Метод fromEntries преобразовывает обратно в объект!!!
+
+let prices = {
+	banana: 1,
+	orange: 2,
+	meat: 4,
+};
+ 
+let doublePrices = Object.fromEntries(
+	// преобразовать в массив, затем map, затем fromEntries обратно объект
+	Object.entries(prices).map(([key, value]) => [key, value * 2])
+);
+ 
+console.log(doublePrices); // 8
