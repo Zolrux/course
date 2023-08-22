f3d09e97bdec60e18ca1028af758bda38e2152ef
Kostya <fubionlaj@gmail.com>
2023-08-22 08:00:30 +0300

    Lecture: async/await

--- js_v2/index.js
+++ js_v2/index.js
@@ -1,30 +1,63 @@
 'use strict';
 
-const funds = [
-	{amount: -1400},
-	{amount: 2400},
-	{amount: -1000},
-	{amount: 500},
-	{amount: 10400},
-	{amount: -11400}
-];
-
-const getPositiveIncomeAmount = (data) => {
-	return data.reduce((acc, {amount}) => {
-		if (amount > 0) acc += amount; 
-		return acc;
-	}, 0);
+const delay = ms => {
+	return new Promise(r => setTimeout(() => r(), ms));
 };
 
-console.log(getPositiveIncomeAmount(funds));
+// delay(2000).then(() => console.log('2 seconds'));
 
-const getTotalIncomeAmount = (data) => {
-	const hasSomeNegativeAmount = data.some(({amount}) => amount < 0);
-	if (hasSomeNegativeAmount) {
-		return data.reduce((acc, {amount}) => acc + amount, 0);
-	} else {
-		getPositiveIncomeAmount(data);
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+// function fetchTodos() {
+// 	console.log('Fetch todo started...');
+// 	return delay(2000)
+// 		.then(() => fetch(url))
+// 		.then(response => response.json());
+// }
+
+// fetchTodos()
+// 	.then(data => {
+// 		console.log(data);
+// 	})
+// 	.catch(error => console.error(error));
+
+function sum(a) {
+	const now = Date.parse(new Date());
+	for (let i = 0; i < 1_000_000_000; i++) {
+		a += i;
 	}
-};
+	const endTime = Date.parse(new Date());
+	const resSeconds =  (endTime - now) / 1000;
+	console.log('Fn time result: ',resSeconds);
+	return a;
+}
 
-console.log(getTotalIncomeAmount(funds));
+async function fetchAsyncTodos() {
+	console.log('Fetch todo started...');
+	try {
+		await delay(2000);
+		const getSum = await sum(5);
+		const response = await fetch(url);
+		if (response.ok) {
+			const data = await response.json();
+			console.log('Data:', data);
+			console.log('sum: ', getSum);
+		}
+	} catch (e) {
+		console.error(e);
+	} finally {
+		console.log('Fetch todo finally ended!');
+	}
+}
+
+fetchAsyncTodos();
+
+function sum2(a, b) {
+	return a + b;
+}
+
+async function calculate() {
+	return await sum2(5, 6);
+}
+
+console.log(calculate());
