8472be30b3dc29d24fda93e283064cc3e6ec523a
Kostya <fubionlaj@gmail.com>
2023-09-06 19:54:19 +0300

    Lecture: try/catch/finally

--- js_v2/index.js
+++ js_v2/index.js
@@ -1,41 +1,15 @@
 'use strict';
 
-// (Глобальное загрязнение)
-// const app = '123'; // Ошибка, так как в lib.js есть такая переменная
+try {
+	console.log('Normal');
+	console.log(a);
+	console.log('result');
+} catch(error) {
+	console.log(error.name);
+	console.log(error.message);
+	console.log(error.stack);
+} finally {
+	console.log('finally!');
+}
 
-const number = 1;
-
-(function() {
-	let number = 2;
-	console.log(number);
-	console.log(number + 3);
-}());
-
-console.log(number);
-
-const user = (function() {
-	const privat = function() {
-		console.log('I am private function');
-	};
-
-	const sum = (a, b) => a + b;
-
-	const getSumInfo = (a, b) => {
-		console.log(`Сумма чисел ${a} и ${b} = ${sum(a, b)}`);
-		return this;
-	};
-
-	return {
-		sayHello: privat,
-		getSumInfo,
-		get sumTest() {
-			console.log('Пример использование суммы чисел 5 и 10');
-			return sum(5, 10);
-		}
-	};
-	
-}());
-
-user.sayHello();
-console.log(user.sumTest);
-user.getSumInfo(5, 10);
+console.log('Still normal');
